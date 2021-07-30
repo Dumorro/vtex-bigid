33605ee4bce1313640d71fd8fb407749c9f85c18
Thiago Dumorro <dumorro@gmail.com>
2021-07-30 10:32:39 -0300

    .

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const request = require('request'); 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const headers = {
   'x-vtex-api-appKey': 'vtexappkey-carrefourbr-SZQQTR',
